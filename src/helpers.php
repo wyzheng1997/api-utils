6a8ab9dd58e2bd9c4310de62268764359c63b91f
wyzheng <wyzheng1997@163.com>
2024-09-27 17:48:36 +0800

    feat: 新增arr2tree函数

---
+++ src/helpers.php
@@ -0,0 +1,25 @@
+<?php
+
+if (! function_exists('arr2tree')) {
+    /**
+     * 数组/集合 转换成树级结构。
+     */
+    function arr2tree(array $list, ?\Closure $transform = null, string $id = 'id', string $pid = 'pid', string $children = 'children'): array
+    {
+        [$map, $tree] = [[], []];
+        foreach ($list as $item) {
+            $map[data_get($item, $id)] = $transform ? call_user_func($transform, $item) : $item;
+        }
+
+        foreach ($list as $item) {
+            if (isset($item[$pid]) && isset($map[$item[$pid]])) {
+                $map[$item[$pid]][$children][] = &$map[$item[$id]];
+            } else {
+                $tree[] = &$map[$item[$id]];
+            }
+        }
+        unset($map);
+
+        return $tree;
+    }
+}
